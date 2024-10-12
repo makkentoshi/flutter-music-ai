98917fa5523a0dc1d40e016e529efbb82cea971b
makkentoshi <pochta.zdraste@mail.ru>
2024-10-12 22:51:45 +0500

    home screen final  + prompt_screen start

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 class HomeScreen extends StatefulWidget {
   final VoidCallback showPromptScreen;
@@ -11,9 +13,146 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold
-    ( body: Center(
-      child: Text("data"),
-    ));
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF330000), Color(0xFF000000)],
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // First Expanded
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: Stack(
+                  children: [
+                    // Container image
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/sonnet.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 40.0),
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          padding: const EdgeInsets.all(5.0),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 0.4,
+                              color: const Color(0xFFFFFFFF),
+                            ),
+                            shape: BoxShape.circle,
+                          ),
+                          child: Container(
+                            height: 110.0,
+                            width: 110.0,
+                            decoration: const BoxDecoration(
+                              color: Color(0xFFFFFFFF),
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                image:
+                                    AssetImage('assets/images/sonnetlogo.png'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            // Second Expanded
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                // Column starts
+                child: Column(
+                  children: [
+                    // RichText
+                    RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                            style: GoogleFonts.inter(
+                              height: 1.3,
+                            ),
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text:
+                                      'AI curated music playlist just for your mood \n',
+                                  style: GoogleFonts.inter(
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.w300,
+                                    color: const Color(0xFFFFFFFF),
+                                  ),
+                                  ),
+                                   TextSpan(
+                                  text:
+                                      'Get Started Now! \n',
+                                  style: GoogleFonts.inter(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: const Color(0xFFFFFFFF),
+                                  )
+                                  )
+                            ],
+                            ),
+                            ),
+                            // Container for the button in padding
+                            Padding(padding: const EdgeInsets.only(top:20.0),
+
+                            // Container for arrow forward in GestureDetector
+
+                            child: GestureDetector(
+                              onTap: widget.showPromptScreen,
+                              child: Container(
+                                padding: EdgeInsets.all(8.0),
+                                decoration: BoxDecoration(color: const Color(0xFFFFCCCC).withOpacity(0.3), shape: BoxShape.circle ),
+                                child: Container(
+                                  height: 50.0,
+                                  width: 50.0,
+                                  padding: const EdgeInsets.all(2.0),
+                                  decoration: BoxDecoration(
+                                    color: const Color(0xFFFFFFFF),
+                                    shape: BoxShape.circle
+                                  ),
+                              
+                                  // Arrow forward centered
+                                  child:  const Center(
+                                    child: Icon(Icons.arrow_forward,),
+                                  ),
+                              
+                              
+                              
+                              
+                                ),
+                              ),
+                            ),
+                            )
+
+                            //
+                  ],
+                ),
+                // Collumn ends
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/prompt_screen.dart
+++ lib/prompt_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PromptScreen extends StatefulWidget {
-   final VoidCallback showHomeScreen;
+  final VoidCallback showHomeScreen;
   const PromptScreen({super.key, required this.showHomeScreen});
 
   @override
@@ -9,10 +10,98 @@ class PromptScreen extends StatefulWidget {
 }
 
 class _PromptScreenState extends State<PromptScreen> {
+  //Genre List
+
+  final List<String> genres = [
+    "Jazz",
+    "Rock",
+    "Amapiano",
+    "Rap",
+    "Hip-Hop",
+    "R&B",
+    "Punk",
+    "Pop",
+    "Blues",
+    "Gospel",
+    "Country",
+    "Metal",
+    "Hip-Life",
+    "Latin"
+  ];
+
+  // Selected Genre List
+
+  final Set<String> _selectedGenres = {};
+
+  // Functoin to toggle genre
+
+  void _onGenreTap(String genre) {
+    setState(() {
+      if (_selectedGenres.contains(genre)) {
+        _selectedGenres.remove(genre);
+      } else {
+        _selectedGenres.add(genre);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      // Container for all content
+
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF330000), Color(0xFF000000)],
+          ),
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        // Padding around content
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50.0, left: 16.0, right: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // First expanded for moods
+
+              const Expanded(
+                  child: Center(
+                      child: Text(
+                "Mood",
+                style: TextStyle(color: Color(0xFFFFFFFF)),
+              ))),
+
+              // Second expanded for genres and submit
+
+              Expanded(
+                  // Column for various genres and submit button in a padding
+                  child: Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                // Column for various genres and submit button
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Genre Text
+                    Text(
+                      "Genre",
+                      style: GoogleFonts.inter(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: const Color(0xFFFFFFFF).withOpacity(0.8)),
+                    )
+                  ],
+                ),
+              ))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
